e15ecdd3d5c6979fd06d1407c47da33f8f47a4e5
Yasin <49972383+YA30N-Remix@users.noreply.github.com>
2022-06-14 16:17:04 +0430

    Change to clean architect

---
+++ Mc2.CrudTest.Presentation/Domain/Common/CustomerFluentValidation.cs
@@ -0,0 +1,23 @@
+﻿
+using FluentValidation;
+
+using Mc2.CrudTest.Presentation.Domain.Entities;
+ //using Mc2.CrudTest.Presentation.Infrastructure;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c2.CrudTest.Application.Validators
+{
+    public class CustomerFluentValidation : AbstractValidator<Customer>
+    {
+        //public CustomerFluentValidation(MyDataBase context)
+        //{   
+        //    RuleFor(x => x.Email).NotNull().WithMessage("Email is Null or Empty!")
+        //        .EmailAddress().WithMessage("Invalid email address format!");                                
+        //}
+    }
+}
